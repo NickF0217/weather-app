e7cf17b30b5f68b21e01ee1709d3fd062f917f99
NickF0217 <ncfoster17@gmail.com>
2022-08-25 23:53:46 -0400

    Can successfully access information from API. Test code successful.

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let baseURL1 = `http://api.openweathermap.org/data/2.5/weather?q=`;
 let baseURL2 = '&APPID=25c6b8239ec277d75611f85f42054af6'
 // console.log(baseURL1 + 'Miami' + baseURL2);
 
+function tempConvertKtoC(k) {
+  let c = k - 273.15;
+  c = c.toFixed(1);
+  return c;
+}
 
 // function testWeather() {
 //     fetch('http://api.openweathermap.org/data/2.5/weather?q=Charlotte&APPID=25c6b8239ec277d75611f85f42054af6',
@@ -13,16 +18,18 @@ let baseURL2 = '&APPID=25c6b8239ec277d75611f85f42054af6'
 //         console.log(result);
 //     })
 // }
-
 // testWeather();
 
 function test2(loc) {
   let newURL = `${baseURL1}${loc}${baseURL2}`;
   fetch(newURL, {mode: 'cors'})
-  .then(function(response) {
-    let result = response.json();
-    console.log(result);
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let temp = data.main.temp;
+    let tempCel = tempConvertKtoC(temp);
+    console.log(`The temperature in ${loc} is ${tempCel} degrees celcius`);
   })
 }
-
 test2('New York');
+
