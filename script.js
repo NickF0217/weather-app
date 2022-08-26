709e970570ef2caa80e405e9b00de0f990627711
NickF0217 <ncfoster17@gmail.com>
2022-08-26 13:18:15 -0400

    Object to hold relevant weather data mostly complete

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=25c6b8239ec277d75611f85f42054af6
+const searchbar = document.getElementById('input');
+const searchBtn = document.getElementById('search-btn');
+const degSymbol = '\u00B0';
+
 let baseURL1 = `http://api.openweathermap.org/data/2.5/weather?q=`;
 let baseURL2 = '&APPID=25c6b8239ec277d75611f85f42054af6'
-// console.log(baseURL1 + 'Miami' + baseURL2);
 
 function tempConvertKtoC(k) {
   let c = k - 273.15;
@@ -44,14 +47,55 @@ async function test3(loc) {
   try {
     const weather = await fetch(newURL, {mode: 'cors'});
     const results = await weather.json();
-    // console.log(results);
+    console.log(results);
     let temp = await results.main.temp;
     let tempConv = await tempConvertKtoF(temp);
-    console.log(`Test 3: The temperature in ${loc} is ${tempConv} degrees fahrenheit`);
+    // console.log(`Test 3: The temperature in ${loc} is ${tempConv} degrees fahrenheit`);
   }
   catch(err) {
     console.log("No match found");
   }
 }
-// test3('Manhattan');
+// test3('Miami');
 
+async function test4(loc) {
+  let newURL = `${baseURL1}${loc}${baseURL2}`;
+  let report = {};
+  try {
+    const weather = await fetch(newURL, {mode: 'cors'});
+    const results = await weather.json();
+    console.log(results);
+
+    let conditions = results.weather[0].description;
+
+    let temp = await results.main.temp;
+    temp = tempConvertKtoF(temp);
+
+    let humid = results.main.humidity;
+
+    let highTemp = results.main.temp_max;
+    highTemp = tempConvertKtoF(highTemp);
+
+    let lowTemp = results.main.temp_min;
+    lowTemp = tempConvertKtoF(lowTemp);
+
+    let windSpd = results.wind.speed;
+
+    report.conditions = conditions;
+    report.temperature = `${temp}${degSymbol}F`;
+    report.humidity = `${humid}%`;
+    report['low temperature'] = lowTemp;
+    report['high temperature'] = highTemp;
+    report.wind = `${windSpd} km/h`
+    console.log(report);
+  }
+  catch(err) {
+    console.log("No match found");
+  }
+}
+test4("Charlotte");
+
+
+searchBtn.addEventListener('click', () => {
+  test4(searchbar.value);
+});
